3e95475fee18fffc2baa167d7cfadbb3530eb164
Germanurgorri-devv <germanurgorri@gmail.com>
2023-07-24 08:33:31 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'D-FlexZone Gym',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
